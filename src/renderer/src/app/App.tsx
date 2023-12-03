82878b567c2449bc2234238ea90966f312822a50
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2023-12-03 11:01:14 +0200

    feat: add login, register, and home routes

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import '@renderer/assets/styles/globa.css'
-import ServerBar from '@renderer/components/ServerBar/ServerBar'
 import Server from '@renderer/views/Server'
 import store from './store'
 import Home from '@renderer/views/Home'
-
+import Register from '@renderer/views/Register'
+import Login from '@renderer/views/Login'
 function App(): JSX.Element {
   return (
     <Provider store={store}>
       <div className=" mt-1 flex">
         <BrowserRouter>
-          <ServerBar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<Home />} />
             <Route path="/server/:id/*" element={<Server />} />
           </Routes>
         </BrowserRouter>
