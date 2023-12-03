d494e7f8a9b7c94d3eb91d78517068682c82de89
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2023-12-03 11:05:07 +0200

    feat: add temporarly navigation buttons

--- src/renderer/src/views/Home.tsx
+++ src/renderer/src/views/Home.tsx
@@ -1,5 +1,36 @@
-export default function Home(): JSX.Element {
+import { useNavigate } from 'react-router-dom'
+
+/**
+ * Component representing the home/DM view.
+ * @component
+ * @returns {React.JSX.Element} The rendered component.
+ */
+export default function Home(): React.JSX.Element {
+  // TODO: Later Will Contain The DM Page
+  const navigate = useNavigate()
   return (
-    <div className="my-1 flex max-h-[calc(100%)] min-h-[calc(100%)] w-[calc(100vw)]  justify-between rounded-2xl bg-soft-white"></div>
+    <div className="my-1 flex max-h-[calc(100%)] min-h-[calc(100%)] w-[calc(100vw)]  justify-between rounded-2xl bg-soft-white">
+      <button
+        onClick={(): void => {
+          navigate('/register')
+        }}
+      >
+        Register
+      </button>
+      <button
+        onClick={(): void => {
+          navigate('/login')
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={(): void => {
+          navigate('/server/1')
+        }}
+      >
+        Server
+      </button>
+    </div>
   )
 }
