66a23f8eb7b2ef065a243a6d5fb6530c66cbff99
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:31:08 +0200

    feat: Clear structure for app component

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,25 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import '@renderer/assets/styles/globa.css'
 import Server from '@renderer/views/Server'
-import store from './store'
 import Home from '@renderer/views/Home'
 import Register from '@renderer/views/Register'
 import Login from '@renderer/views/Login'
-function App(): JSX.Element {
+
+/**
+ * Main application component that handles routing and renders primary views.
+ *
+ * @returns {React.JSX.Element} The rendered component hierarchy.
+ */
+function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <div className=" mt-1 flex">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/*" element={<Home />} />
-            <Route path="/server/:id/*" element={<Server />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </Provider>
+    <div className={`mt-1 flex`}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Home />} />
+          <Route path="/server/:id/*" element={<Server />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
