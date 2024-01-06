0cb4a512d9ac4440299663d60288118c1087daa5
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:47:39 +0200

    fix: Inconsistent docs

--- src/renderer/src/features/register/SubComponents/StepThree.tsx
+++ src/renderer/src/features/register/SubComponents/StepThree.tsx
@@ -1,57 +1,56 @@
 import { ErrorMessage, Field, useFormikContext } from 'formik'
 import { useState } from 'react'
-
-import { StepThreeValues } from '../RegisterForm.d'
+import { RegisterStepThreeValues } from '@renderer/env'
 
 /**
- * Component representing the third step of the registration form.
- * @component
- * @returns {React.JSX.Element} The rendered component.
+ * Register form step three/three component
+ * @returns {React.JSX.Element}  renderer component.
  */
 export default function StepThree(): React.JSX.Element {
-  const { errors, touched, handleBlur, validateField, values } = useFormikContext<StepThreeValues>()
+  const { errors, touched, handleBlur, validateField, values } =
+    useFormikContext<RegisterStepThreeValues>()
 
-  // State to track field errors and apply delayed clearin
   const [fieldErrors, setFieldErrors] = useState({
     photo: false,
     status: false
   })
 
   /**
-   * Handles the blur event for form fields.
-   * @param {Event} e - The blur event.
-   * @param {string} fieldName - The name of the field being blurred.
+   * Override default formik onBlur Behaviour
+   * @param {Event} e
+   * @param {string} fieldName
    * @returns {void}
    */
   const handleFieldBlur = (e: Event, fieldName: string): void => {
     handleBlur(e)
     validateField(fieldName)
 
-    // Set the field error to true and clear it after a delay
     setFieldErrors((prevFieldErrors) => ({ ...prevFieldErrors, [fieldName]: true }))
     setTimeout(() => {
       setFieldErrors((prevFieldErrors) => ({ ...prevFieldErrors, [fieldName]: false }))
     }, 2000)
   }
 
-  // Handle image errors
   const handleImageError = (fieldName: string): void => {
     setFieldErrors((prevFieldErrors) => ({ ...prevFieldErrors, [fieldName]: true }))
   }
 
   return (
-    <div className="w-[calc(30vw)] flex-col items-center justify-center text-default-txt">
-      <h1 className="p-4 text-center text-default-txt">Let People Know You</h1>
-      <div className="flex items-center justify-center">
+    <div className={`w-[calc(30vw)] flex-col items-center justify-center text-default-txt`}>
+      <h1 className={`p-4 text-center text-default-txt`}>Let People Know You</h1>
+      <div className={`flex items-center justify-center`}>
         {values.photo && !fieldErrors.photo ? (
           <img
             src={values.photo}
             alt="User Photo"
-            className="h-24 w-24 rounded-lg"
             onError={(): void => handleImageError('photo')}
+            className={`h-24 w-24 rounded-lg`}
           />
         ) : (
-          <div className="flex h-24 w-24 items-center justify-center rounded-lg bg-hard-white p-3 text-center text-xs">
+          <div
+            className={`flex h-24 w-24 items-center justify-center 
+            rounded-lg bg-hard-white p-3 text-center text-xs`}
+          >
             Photo will appear here
           </div>
         )}
@@ -64,10 +63,10 @@ export default function StepThree(): React.JSX.Element {
         onBlur={(e: Event): void => {
           handleFieldBlur(e, 'photo')
         }}
-        className="form-input m-1 w-[calc(100%)]"
+        className={`critch-form-input m-1 w-[calc(100%)]`}
       />
       {fieldErrors.photo && touched.photo && errors.photo && (
-        <ErrorMessage name="photo" component="div" className={`error-message`} />
+        <ErrorMessage name="photo" component="div" className={`critch-error-message`} />
       )}
 
       <Field
@@ -78,15 +77,16 @@ export default function StepThree(): React.JSX.Element {
         onBlur={(e: Event): void => {
           handleFieldBlur(e, 'status')
         }}
-        className="form-input m-1 w-[calc(100%)]"
+        className={`critch-form-input m-1 w-[calc(100%)]`}
       />
       {fieldErrors.status && touched.status && errors.status && (
-        <ErrorMessage name="status" component="div" className={`error-message`} />
+        <ErrorMessage name="status" component="div" className={`critch-error-message`} />
       )}
-      <div className="flex justify-center">
+      <div className={`flex justify-center`}>
         <button
           type="submit"
-          className="m-0.5 rounded-md bg-soft-purble p-1.5 text-sm text-original-white hover:bg-soft-purble/80"
+          className={`m-0.5 rounded-md bg-soft-purble p-1.5 
+          text-sm text-original-white hover:bg-soft-purble/80`}
         >
           Signup
         </button>
--- src/renderer/src/features/register/SubComponents/StepTwo.tsx
+++ src/renderer/src/features/register/SubComponents/StepTwo.tsx
@@ -1,34 +1,30 @@
+import { RegisterStepTwoValues } from '@renderer/env'
 import { ErrorMessage, FormikErrors, useFormikContext } from 'formik'
 import { useState } from 'react'
 import PhoneInputWithCountrySelect from 'react-phone-number-input'
 import 'react-phone-number-input/style.css'
 
-import { StepTwoValues } from '../RegisterForm.d'
-
 /**
- * Component representing the second step of the registration form.
- * @component
- * @returns {React.JSX.Element} The rendered component.
+ * Register form step two/three component
+ * @returns {React.JSX.Element}  renderer component.
  */
 export default function StepTwo(): React.JSX.Element {
   const { errors, touched, handleBlur, validateField, setFieldValue } =
-    useFormikContext<StepTwoValues>()
+    useFormikContext<RegisterStepTwoValues>()
 
-  // State to track field errors and apply delayed clearing
   const [fieldErrors, setFieldErrors] = useState({
     phone: false
   })
 
   /**
-   * Handles the blur event for the phone number field.
-   * @param {string} fieldName - The name of the field being blurred.
+   * Override default formik onBlur Behaviour
+   * @param {string} fieldName
    * @returns {void}
    */
   const handleFieldBlur = (fieldName: string): void => {
     handleBlur({ target: { name: fieldName } })
     validateField(fieldName)
 
-    // Set the field error to true and clear it after a delay
     setFieldErrors((prevFieldErrors) => ({ ...prevFieldErrors, [fieldName]: true }))
     setTimeout(() => {
       setFieldErrors((prevFieldErrors) => ({ ...prevFieldErrors, [fieldName]: false }))
@@ -36,20 +32,20 @@ export default function StepTwo(): React.JSX.Element {
   }
 
   return (
-    <div className="w-[calc(30vw)] text-default-txt">
-      <h1 className="p-4 text-center text-default-txt">Enter Your Phone Number</h1>
+    <div className={`w-[calc(30vw)] text-default-txt`}>
+      <h1 className={`p-4 text-center text-default-txt`}>Enter Your Phone Number</h1>
       <PhoneInputWithCountrySelect
         placeholder="Enter phone number"
         id="phone"
         name="phone"
-        onChange={(value: string): Promise<void | FormikErrors<StepTwoValues>> =>
+        onChange={(value: string): Promise<void | FormikErrors<RegisterStepTwoValues>> =>
           setFieldValue('phone', value)
         }
         onBlur={(): void => handleFieldBlur('phone')}
-        className="form-input my-4 w-[calc(100%)] bg-original-white p-2"
+        className={`critch-form-input my-4 w-[calc(100%)] bg-original-white p-2`}
       />
       {fieldErrors.phone && touched.phone && errors.phone && (
-        <ErrorMessage name="phone" component="div" className="error-message" />
+        <ErrorMessage name="phone" component="div" className={`critch-error-message`} />
       )}
     </div>
   )
