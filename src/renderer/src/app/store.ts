ba3f528c1a00d012e4b3baecbe559a81716fd3b9
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:28:44 +0200

    feat: Login state reducer added

--- src/renderer/src/app/store.ts
+++ src/renderer/src/app/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
-import serverBarReducer from '@renderer/components/ServerBar/serverBarReducer'
+import serverBarReducer from '@renderer/features/server/ServerBar/serverBarReducer'
 import channelsBarReducer from '@renderer/features/server/ChannelsBar/channelsBarReducer'
+import loginReducer from '@renderer/features/login/loginReducer'
 
+/**
+ * Creates and configures the Redux store for managing application state.
+ *
+ * @returns The configured Redux store.
+ */
 const store = configureStore({
   reducer: {
     serverBar: serverBarReducer,
-    channelsBar: channelsBarReducer
+    channelsBar: channelsBarReducer,
+    login: loginReducer
   }
 })
 export type RootState = ReturnType<typeof store.getState>
