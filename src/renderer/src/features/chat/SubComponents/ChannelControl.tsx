f97f041b4a0b707fe557fc29dea0695bcec91ab8
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:43:18 +0200

    fix: Clear code

--- src/renderer/src/features/chat/SubComponents/ChannelControl.tsx
+++ src/renderer/src/features/chat/SubComponents/ChannelControl.tsx
@@ -1,11 +1,11 @@
-import { faGear, faVideo, faPhone, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { faGear, faPhone, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Divider from '@renderer/components/Divider//Divider'
 
 /**
- * @property { channelName } @type string
- * @returns {ChannelControl} @type React.JSX.Element
- * @description The channel title cnd control component
+ * Channel control component
+ * @property {string} channelName - Active channel name
+ * @returns {React.JSX.Element} renderer component.
  */
 export default function ChannelControl({
   channelName
@@ -14,22 +14,26 @@ export default function ChannelControl({
 }): React.JSX.Element {
   return (
     <>
-      <div className="flex items-center justify-between">
-        <h1 className="p-3 text-xl text-default-txt/75 ">
-          <FontAwesomeIcon icon={faCirclePlay} className="px-1" />
+      <div className={`flex items-center justify-between`}>
+        <h1 className={`p-3 text-xl text-default-txt/75`}>
+          <FontAwesomeIcon icon={faCirclePlay} className={`px-1`} />
           {channelName}
         </h1>
 
-        <div className="px-4 text-sm text-original-white">
-          <button className="mx-1 inline-flex h-7 w-7 items-center justify-center rounded-full bg-soft-purble/70 p-2 duration-200 hover:rotate-180 hover:bg-soft-purble/100">
+        <div className={`px-4 text-sm text-original-white`}>
+          <button
+            className={`mx-1 inline-flex h-7 w-7 items-center justify-center 
+            rounded-full bg-soft-purble/70 p-2 duration-200 hover:rotate-180
+          hover:bg-soft-purble/100`}
+          >
             <FontAwesomeIcon icon={faPhone} />
           </button>
 
-          <button className="mx-1 inline-flex h-7 w-7 items-center justify-center rounded-full bg-soft-purble/70 p-2 duration-200 hover:rotate-180 hover:bg-soft-purble/100">
-            <FontAwesomeIcon icon={faVideo} />
-          </button>
-
-          <button className="mx-1 inline-flex h-7 w-7 items-center justify-center rounded-full bg-soft-purble/70 p-2 duration-200 hover:rotate-180 hover:bg-soft-purble/100">
+          <button
+            className={`mx-1 inline-flex h-7 w-7 items-center justify-center 
+            rounded-full bg-soft-purble/70 p-2 duration-200 hover:rotate-180
+          hover:bg-soft-purble/100`}
+          >
             <FontAwesomeIcon icon={faGear} />
           </button>
         </div>
