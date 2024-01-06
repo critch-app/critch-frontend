92622e35c66cf7663517812d15495b58d1722d9f
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:26:46 +0200

    fix: Clear structure for app component

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
