ffa2f443911b881197292f5148a251a08de2320b
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-06 02:38:47 +0200

    feat: API integration

--- src/renderer/src/views/Login.tsx
+++ src/renderer/src/views/Login.tsx
@@ -1,36 +1,89 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
-import AppIcon from '@renderer/components/ServerBar/SubComponents/AppIcon'
+import AppIcon from '@renderer/components/AppIcon/AppIcon'
 import LoginForm from '@renderer/features/login/LoginForm'
-
+import { loginMut } from '@renderer/api/query/user'
+import { setLoggedInUserID, setLoggedInUserToken } from '@renderer/features/login/loginReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@renderer/app/store'
+import { useEffect, useState } from 'react'
+import Error from '@renderer/components/Error/Error'
+import Loading from '@renderer/components/Loading/Loading'
 /**
- * Component representing the login view.
- * @component
- * @returns {React.JSX.Element} The rendered component.
+ * Component represents the login view
+ * @returns {React.JSX.Element} rendered component.
  */
+
 export default function Login(): React.JSX.Element {
+  const [apiError, setApiError] = useState('')
+  const navigate = useNavigate()
+
+  const mut = loginMut(() => {
+    navigate('/')
+  })
+
+  const loggedInUserId = useSelector((state: RootState) => state.login.loggedInUserID)
+  const loggedInUserToken = useSelector((state: RootState) => state.login.loggedInUserToken)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (loggedInUserId && loggedInUserToken) {
+      navigate('/')
+    }
+  })
+
   /**
-   * Handles the form submission.
-   * @param {object} formData - The form data.
+   * On submit form handler.
+   * @param {any} {values} - The form data.
    * @returns {void}
    */
-  const handleSubmit = ({ ...formData }: any): void => {
-    console.log(`form submitted with values`, formData)
+  const handleSubmit = async (values: any): Promise<void> => {
+    let res
+    try {
+      res = await mut.mutateAsync(values)
+      dispatch(setLoggedInUserID(res.data.user_id))
+      dispatch(setLoggedInUserToken(res.data.token))
+    } catch (error: any) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setApiError(error.response.data.message)
+      } else {
+        setApiError('An unexpected error occurred')
+      }
+    }
   }
 
+  // Handle error state
+  if (mut.status == 'error') {
+    return <Error error={apiError} />
+  }
+
+  // Handle loading state
+  if (mut.status == 'pending') {
+    return (
+      <div>
+        <Loading size={170} />
+      </div>
+    )
+  }
+
+  // Component
   return (
-    <div className="relative max-h-[calc(95vh)] min-h-[calc(95vh)] w-full flex-col items-center justify-center">
-      <div className="absolute left-[calc(50%)] top-[calc(50%)] flex h-[calc(70%)] w-[calc(35vw)] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-2xl bg-soft-white">
-        <div className="h-28 w-28">
-          <AppIcon />
+    <div
+      className={`relative max-h-[calc(95vh)] min-h-[calc(95vh)] w-full flex-col items-center justify-center`}
+    >
+      <div
+        className={`absolute left-[calc(50%)] top-[calc(50%)] flex h-[calc(70%)] w-[calc(35vw)] 
+        -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-2xl bg-soft-white`}
+      >
+        <div className={`h-28 w-28`}>
+          <AppIcon width={`w-32`} height={`h-32`} />
         </div>
         <LoginForm onSubmit={handleSubmit} />
-        <p className="m-2 text-sm text-extra-gray">
+        <p className={`m-2 text-sm text-extra-gray`}>
           New To Critch?
           <Link
             to="/register"
-            className="px-1 font-semibold text-soft-purble/80 underline hover:text-soft-purble"
+            className={`px-1 font-semibold text-soft-purble/80 underline hover:text-soft-purble`}
           >
             Create Account Here!
           </Link>
--- src/renderer/src/views/Register.tsx
+++ src/renderer/src/views/Register.tsx
@@ -1,44 +1,91 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
-import AppIcon from '@renderer/components/ServerBar/SubComponents/AppIcon'
+import AppIcon from '@renderer/components/AppIcon/AppIcon'
 import RegisterForm from '@renderer/features/register/RegisterForm'
+import { registerMut } from '@renderer/api/query/user'
+import { RootState } from '@renderer/app/store'
+import { useSelector } from 'react-redux'
+import Loading from '@renderer/components/Loading/Loading'
+import Error from '@renderer/components/Error/Error'
 
 /**
- * Component representing the register view.
- * @component
- * @returns {React.JSX.Element} The rendered component.
+ * Component represents the signup view
+ * @returns {React.JSX.Element} rendered component.
  */
 
 export default function Register(): React.JSX.Element {
   const [step, setStep] = useState(1)
+  const [apiError, setApiError] = useState('')
+  const navigate = useNavigate()
+
+  const mut = registerMut(() => {
+    navigate('/login')
+  })
+
+  const loggedInUserId = useSelector((state: RootState) => state.login.loggedInUserID)
+  const loggedInUserToken = useSelector((state: RootState) => state.login.loggedInUserToken)
+
+  useEffect(() => {
+    if (loggedInUserId && loggedInUserToken) {
+      navigate('/')
+    }
+  })
 
   /**
-   * Handles the form submission.
-   * @param {object} formData - The form data.
+   * On submit form handler.
+   * @param {any} {values} - The form data.
    * @returns {void}
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleSubmit = ({ confirm_password, ...formData }: any): void => {
-    formData.photo =
-      formData.photo ||
+  const handleSubmit = async ({ confirm_password, ...values }: any): Promise<void> => {
+    values.photo =
+      values.photo ||
       'https://t4.ftcdn.net/jpg/03/59/58/91/360_F_359589186_JDLl8dIWoBNf1iqEkHxhUeeOulx0wOC5.jpg'
-    console.log(`form submitted with values`, formData)
+    try {
+      await mut.mutateAsync(values)
+    } catch (error: any) {
+      if (error.response && error.response.data && error.response.data.message) {
+        setApiError(error.response.data.message)
+      } else {
+        setApiError('An unexpected error occurred')
+      }
+    }
   }
 
+  // Handle error state
+  if (mut.status == 'error') {
+    return <Error error={apiError} />
+  }
+
+  // Handle loading state
+  if (mut.status == 'pending') {
+    return (
+      <div>
+        <Loading size={170} />
+      </div>
+    )
+  }
+
+  // Component
   return (
-    <div className="relative max-h-[calc(95vh)] min-h-[calc(95vh)] w-full flex-col items-center justify-center">
-      <div className="absolute left-[calc(50%)] top-[calc(50%)] flex h-[calc(70%)] w-[calc(35vw)] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-2xl bg-soft-white">
+    <div
+      className={`relative max-h-[calc(95vh)] min-h-[calc(95vh)] w-full flex-col items-center justify-center`}
+    >
+      <div
+        className={`absolute left-[calc(50%)] top-[calc(50%)] flex h-[calc(70%)] w-[calc(35vw)] 
+        -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-2xl bg-soft-white`}
+      >
         <div className="h-28 w-28">
-          <AppIcon />
+          <AppIcon width={`w-32`} height={`h-32`} />
         </div>
         <RegisterForm currentStep={step} setStep={setStep} onSubmit={handleSubmit} />
-        <p className="m-2 text-sm text-extra-gray">
+        <p className={`m-2 text-sm text-extra-gray`}>
           Already have an account?
           <Link
             to="/login"
-            className="px-1 font-semibold text-soft-purble/70 underline hover:text-soft-purble"
+            className={`px-1 font-semibold text-soft-purble/70 underline hover:text-soft-purble`}
           >
             Login Here!
           </Link>
