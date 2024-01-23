25985f156fe95d509b0de188b528666c0debe259
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-23 14:45:11 +0200

    feat: expose the new handler and ipcRenderer specific APIs to the main world

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
-  writeToClipboard: async (content: string): Promise<void> =>
-    ipcRenderer.send('write-to-clipboard', content),
-  showNotifications: async (title: string, body: string): Promise<void> =>
+  writeToClipboard: async (content: string): Promise<void> => {
+    ipcRenderer.send('write-to-clipboard', content)
+  },
+  showNotifications: async (title: string, body: string): Promise<void> => {
     ipcRenderer.send('show-notification', title, body)
+  },
+  generateInvitation: async (serverId: string): Promise<void> => {
+    ipcRenderer.send('generate-invitation', serverId)
+  }
+})
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    send: (channel: string, ...args: any) => ipcRenderer.send(channel, ...args),
+    on: (channel: string, listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void) =>
+      ipcRenderer.on(channel, listener)
+  }
 })
