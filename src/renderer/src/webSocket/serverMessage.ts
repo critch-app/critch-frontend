e6297edb50cb933dc99523d0c7c0b4e56d8eea0b
hussein119 <hussein.abdelkader119@gmail.com>
2024-01-26 01:14:56 +0200

    feat: implement close connection function to server message socket

--- src/renderer/src/webSocket/serverMessage.ts
+++ src/renderer/src/webSocket/serverMessage.ts
@@ -1,24 +1,29 @@
 /* eslint-disable prettier/prettier */
-import useWebSocket from 'react-use-websocket'
+import useWebSocket, { ReadyState } from 'react-use-websocket'
 
 const baseWsUrl = 'ws://localhost:8080'
 
 const wsUrl = `${baseWsUrl}/v1/messaging-service`
 
-const handleServerMessageSocket = (): ((
-  channelId: string,
-  content: string,
-  serverId: string
-) => void) => {
-  const { sendJsonMessage } = useWebSocket(wsUrl, {
-    queryParams: {}
-  })
+const createServerMessageSocket = (): {
+  sendMessage: (channelId: string, content: string, serverId: string) => void
+  closeConnection: () => void
+} => {
+  const { sendJsonMessage, readyState } = useWebSocket(wsUrl)
 
-  return (channelId: string, content: string, serverId: string) => {
+  const sendMessage = (channelId: string, content: string, serverId: string): void => {
     if (sendJsonMessage) {
       sendJsonMessage({ channelId, content, serverId })
     }
   }
+
+  const closeConnection = (): void => {
+    if (readyState === ReadyState.OPEN) {
+      sendJsonMessage({ type: 'disconnect' })
+    }
+  }
+
+  return { sendMessage, closeConnection }
 }
 
-export default handleServerMessageSocket
+export default createServerMessageSocket
