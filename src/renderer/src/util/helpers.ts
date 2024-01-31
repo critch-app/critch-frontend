b452a70b2cb53c2f5c522cc7f19f111b068bdfe9
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-01-31 13:10:13 +0200

    feat: new event types

--- src/renderer/src/util/helpers.ts
+++ src/renderer/src/util/helpers.ts
@@ -7,3 +7,33 @@ export const scrollToBottom = (windowRef: React.RefObject<HTMLDivElement> | null
     windowRef.current.scrollTop = scrollHeight - clientHeight
   }
 }
+
+export const getUserMedia = async (voice: boolean, video: boolean): Promise<MediaStream> => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    frameRate: 30
+  }
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: voice,
+    video: video ? videoConstraints : false
+  })
+  return stream
+}
+
+export const getUserScreen = async (voice: boolean): Promise<MediaStream> => {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    frameRate: 30,
+    mandatory: {
+      chromeMediaSource: 'desktop'
+    }
+  }
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: voice,
+    video: videoConstraints
+  })
+  return stream
+}
