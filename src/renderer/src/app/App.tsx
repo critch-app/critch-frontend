173e9571b93a9c4ca3772cd7f319a7e2274ff2ae
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-02-03 23:53:00 +0200

    feat: integrate the new pip functionality

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -8,20 +8,32 @@ import { useGlobalEventListeners } from '@renderer/hooks/useGlobalEventListners'
 import Error from '@renderer/components/Error/Error'
 import { useConnectionState } from '@renderer/hooks/useConnectionState'
 import UnrecoveredError from '@renderer/views/UnrecoveredError'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useWebSocket } from '@renderer/hooks/useWebSocket'
 import { RootState } from './store'
 import { useSelector } from 'react-redux'
 import { useWebSocketProvider } from '@renderer/hooks/useWebSocketProvider'
 import ErrorBoundary from '@renderer/components/ErrorBoundary/ErrorBoundary'
+import Pip from '@renderer/components/Modal/Pip'
+import PipModal from '@renderer/features/stream/SubComponents/PipModal'
+import { useGlobalPipProvider } from '@renderer/hooks/useGlobalPipProvider'
+import { Client, RemoteStream } from 'ion-sdk-js'
+import { IonSFUJSONRPCSignal } from 'ion-sdk-js/lib/signal/json-rpc-impl'
 
 function App(): React.JSX.Element {
   const userToken = useSelector((state: RootState) => state.login.userToken)
+  const joinedChannel = useSelector((state: RootState) => state.meeting.joinedChannel)
+  const pip = useSelector((state: RootState) => state.meeting.pip)
   const { isError, error } = useGlobalEventListeners()
   const { isConnectionError, connectionError } = useConnectionState()
   const navigate = useNavigate()
   const { isWsConnectionError, ws } = useWebSocket(userToken)
   const WSProvider = useWebSocketProvider()
+  const GlopalPipProvider = useGlobalPipProvider()
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null)
+  const [remoteStreams, setRemoteStreams] = useState<RemoteStream[]>([])
+  const [client, setClient] = useState<Client | null>(null)
+  const [signal, setSignal] = useState<IonSFUJSONRPCSignal | null>(null)
 
   useEffect(() => {
     if (isWsConnectionError) {
@@ -42,16 +54,34 @@ function App(): React.JSX.Element {
   return (
     <ErrorBoundary>
       <WSProvider.Provider value={ws}>
-        <div className={`mt-1 flex`}>
-          {isError && <Error error={error} reset={null} />}
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/*" element={<Home />} />
-            <Route path="/server/:id/*" element={<Server />} />
-            <Route path="/un-recovered-error" element={<UnrecoveredError />} />
-          </Routes>
-        </div>
+        <GlopalPipProvider.Provider
+          value={{
+            mediaStream,
+            setMediaStream,
+            remoteStreams,
+            setRemoteStreams,
+            client,
+            setClient,
+            signal,
+            setSignal
+          }}
+        >
+          <div className={`mt-1 flex`}>
+            {isError && <Error error={error} reset={null} />}
+            {joinedChannel && pip && (
+              <Pip>
+                <PipModal />
+              </Pip>
+            )}
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/*" element={<Home />} />
+              <Route path="/server/*" element={<Server />} />
+              <Route path="/un-recovered-error" element={<UnrecoveredError />} />
+            </Routes>
+          </div>
+        </GlopalPipProvider.Provider>
       </WSProvider.Provider>
     </ErrorBoundary>
   )
